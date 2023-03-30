49694c47f904569fdf250b808978352e200cb765
mattjayyoung <matthew.young@live.ca>
2023-03-30 12:30:14 -0400

    march 30th codewars

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
+// Get the Middle Character - 7kyu
+
 /* You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters. */
 
 function getMiddle(str){
@@ -9,5 +11,41 @@ function getMiddle(str){
     }
 }
 
+// Tribonacci Sequence - 6kyu
+
+/*
+
+As the name may already reveal, it works basically like a Fibonacci, but summing the last 3 (instead of 2) numbers of the sequence to generate the next.
+
+So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting input (AKA signature), we have this sequence:
+
+[1, 1 ,1, 3, 5, 9, 17, 31, ...]
+
+But what if we started with [0, 0, 1] as a signature? As starting with [0, 1] instead of [1, 1] basically shifts the common Fibonacci sequence by once place, you may be tempted to think that we would get the same sequence shifted by 2 places, but that is not the case and we would get:
+
+[0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
+Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
+
+Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
 
 
+*/
+
+function tribonacci(signature,n){
+    let newSequence = signature
+    
+    if (n === 0){
+        return []
+    } else if (n === 1) {
+        return [newSequence[0]]
+
+    } else {
+
+    for (let i = 3; i < n; i++){
+        signature[i] = signature[i - 3] + signature[i - 2] + signature[i - 1]
+        newSequence.push(signature[i])
+        newSequence.pop()
+    }
+    return newSequence
+}
+  }
