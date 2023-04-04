dfcbbd83fb19757af8ccb25b3b84ae8425271002
Matt Young <matthew.young@live.ca>
2023-04-04 15:41:50 -0400

    april 4th codewars complete

--- main.js
+++ main.js
@@ -107,3 +107,19 @@ function reverseWords(str) {
     return str.split(' ').map(word => word.split('').reverse().join('')).join(' ')
   }
 
+// Between Extremes - 7kyu (Apr 4th)
+
+/*
+
+Given an array of numbers, return the difference between the largest and smallest values.
+
+The array will contain a minimum of two elements. Input data range guarantees that max-min will cause no integer overflow.
+
+*/
+
+function betweenExtremes(numbers) {
+  const sortedNum = numbers.sort((a,b) => a - b)
+
+  return sortedNum[sortedNum.length - 1] - sortedNum[0]
+  
+}
