1bb857320e4f6265881749349cae86fe6fe18574
Matt Young <matthew.young@live.ca>
2023-04-07 10:42:55 -0400

    apr 7th codewars complete

--- main.js
+++ main.js
@@ -141,3 +141,55 @@ function isItANum(str) {
   const nums = str.split(' ').join('').split('').filter(char => char >= 0).join('')
   return nums[0] === '0' && nums.length === 11 ? nums : 'Not a phone number'
 }
+
+// Cats and shelves - 7kyu (Apr 7th)
+
+/*
+
+The cat can jump either one or three shelves at a time: from shelf i to shelf i+1 or i+3 (the cat cannot climb on the shelf directly above its head), according to the illustration:
+
+                 ┌────────┐
+                 │-6------│
+                 └────────┘
+┌────────┐       
+│------5-│        
+└────────┘  ┌─────► OK!
+            │    ┌────────┐
+            │    │-4------│
+            │    └────────┘
+┌────────┐  │
+│------3-│  │     
+BANG!────┘  ├─────► OK! 
+  ▲  |\_/|  │    ┌────────┐
+  │ ("^-^)  │    │-2------│
+  │ )   (   │    └────────┘
+┌─┴─┴───┴┬──┘
+│------1-│
+└────────┘
+
+Input
+Start and finish shelf numbers (always positive integers, finish no smaller than start)
+
+Task
+Find the minimum number of jumps to go from start to finish
+
+Example
+Start 1, finish 5, then answer is 2 (1 => 4 => 5 or 1 => 2 => 5)
+
+*/
+
+function solution(start, finish) {
+  let jumpCount = 0
+  let i = start
+  
+  while (i < finish){
+    
+    if (finish - i >= 3){
+    i += 3
+    } else if (finish - i >= 1) {
+      i += 1
+    } 
+    jumpCount++
+  }
+  return jumpCount
+}
