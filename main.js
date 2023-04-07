7d8cc6b259487085a205831633f408272e021e94
Matt Young <matthew.young@live.ca>
2023-04-06 22:23:40 -0400

    april 6th codewars completed

--- main.js
+++ main.js
@@ -123,3 +123,21 @@ function betweenExtremes(numbers) {
   return sortedNum[sortedNum.length - 1] - sortedNum[0]
   
 }
+
+// Fix My Phone Numbers! - 7kyu (Apr 6th)
+
+/*
+
+Phone numbers are stored as strings and comprise 11 digits, eg '02078834982' and must always start with a 0. 
+
+However, something strange has happened and now all of the phone numbers contain lots of random characters, whitespace and some are not phone numbers at all!
+
+Given a string, you must decide whether or not it contains a valid phone number. If it does, return the corrected phone number as a string ie. '02078834982' with no whitespace or special characters, else return "Not a phone number".
+
+*/
+
+function isItANum(str) {
+
+  const nums = str.split(' ').join('').split('').filter(char => char >= 0).join('')
+  return nums[0] === '0' && nums.length === 11 ? nums : 'Not a phone number'
+}
