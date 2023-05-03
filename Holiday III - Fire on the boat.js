faa779c3d4e8dd87be379c51e89bc8cae4ee7eab
Matt <70331360+mattjayyoung@users.noreply.github.com>
2023-05-02 21:30:07 -0400

    may 2nd complete

---
+++ Holiday III - Fire on the boat.js
@@ -0,0 +1,10 @@
+// Holiday III - Fire on the boat (7kyu) - May 2nd
+
+function fireFight(s){
+  return s.replaceAll('Fire', '~')
+}
+
+
+fireFight("Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast") // "Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast"
+fireFight("Mast Deck Engine Water Fire") // "Mast Deck Engine Water ~~"
+fireFight("Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain") // "~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain"
