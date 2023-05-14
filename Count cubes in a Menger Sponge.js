d34cb46e804add314b8453532a7fea7c3945735c
Matt <70331360+mattjayyoung@users.noreply.github.com>
2023-05-14 12:22:42 -0400

    May 14th complete

---
+++ Count cubes in a Menger Sponge.js
@@ -0,0 +1,14 @@
+//Count cubes in a Menger Sponge - 7kyu (May 14th)
+
+function calc_ms(n) {
+   return 20 ** n
+  }
+
+  calc_ms(1)// 20
+
+calc_ms(0)// 1
+calc_ms(1)// 20
+calc_ms(2)// 400
+calc_ms(3)// 8000
+calc_ms(4)// 160000
+calc_ms(6)// 64000000
