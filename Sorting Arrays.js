329185ea003d7b3dc38a23ea3fc4f27d1f8a84ba
Matt <70331360+mattjayyoung@users.noreply.github.com>
2023-06-10 10:23:45 -0400

    June 10th complete

---
+++ Sorting Arrays.js
@@ -0,0 +1,19 @@
+function sortArray(a1, a2) {
+    let firstInitialArray = a1.map(el => el[0])
+    let sortedArray = []
+    
+
+    for (let i = 0; i < firstInitialArray.length; i++){
+        for (let j = 0; j < a2.length; j++){
+            if (firstInitialArray[i] === a2[j][0]){
+                sortedArray.push(a2[j])
+            }
+        }
+    }
+    return sortedArray
+
+}
+
+let a1 = ['giraffe', 'orangutan', 'impala', 'elephant', 'rhino'];
+let a2 = ['rattlesnake', 'eagle', 'geko', 'iguana', 'octopus'];
+sortArray(a1, a2)// ['geko', 'octopus', 'iguana', 'eagle', 'rattlesnake'];
