a9e001fe1524a2b2f538bcb66a7ce023e0b43e55
Matt <70331360+mattjayyoung@users.noreply.github.com>
2023-09-16 10:01:59 -0400

    September 16th complete

---
+++ lost without a map.js
@@ -0,0 +1,10 @@
+// Beginner - Lost Without a Map - 8kyu (September 16th)
+
+function maps(arr){
+  return arr.map(num => num * 2)
+
+}
+
+maps([1, 2, 3]) // [2, 4, 6]
+maps([4, 1, 1, 1, 4]) // [8, 2, 2, 2, 8]); 
+maps([2, 2, 2, 2, 2, 2]) // [4, 4, 4, 4, 4, 4]); 
