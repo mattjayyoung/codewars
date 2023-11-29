a547ca68df4ef90cb52430975ddc85340994a07a
Matt <70331360+mattjayyoung@users.noreply.github.com>
2023-11-28 22:59:46 -0500

    November 28th complete

---
+++ 8kyu/Who ate the cookie.js
@@ -0,0 +1,11 @@
+// Who ate the cookie? - 8kyu (November 28th)
+
+function cookie(x){
+  if (typeof x === 'string'){
+    return 'Who ate the last cookie? It was Zach!'
+  } else if (typeof x === 'number'){
+    return 'Who ate the last cookie? It was Monica!'
+  } else {
+    return 'Who ate the last cookie? It was the dog!'
+  }
+}
