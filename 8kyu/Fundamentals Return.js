1d4d84e7824890487e3a17cc345249ea154b56f7
Matt <70331360+mattjayyoung@users.noreply.github.com>
2023-12-15 19:16:22 -0500

    December 15th complete

---
+++ 8kyu/Fundamentals Return.js
@@ -0,0 +1,25 @@
+// Fundamentals: Return - 8kyu (December 15th)
+
+function add(a,b){
+    return a + b
+}
+
+function divide(a,b){
+    return a / b
+}
+
+function multiply(a,b){
+    return a * b
+}
+
+function mod(a,b){
+    return a % b
+}
+   
+function exponent(a,b){
+    return a ** b
+}
+    
+function subt(a,b){
+    return a - b
+}
