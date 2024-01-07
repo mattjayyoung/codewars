aa79452ef383a4ee3b34387243fae87182efe2eb
Matt <70331360+mattjayyoung@users.noreply.github.com>
2024-01-06 22:48:32 -0500

    January 6th complete

---
+++ basic data types - booleans and conditional statements.js
@@ -0,0 +1,9 @@
+// Basic data types--Boolean and conditional statements if..else - 8kyu (January 6th)
+
+function trueOrFalse(val){
+  if (!val){
+    return 'false';             
+ } else{
+    return 'true'; 
+   }
+  }
