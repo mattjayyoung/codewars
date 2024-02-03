eefb1c25520366e34da28e10b00cbb94713958b9
Matt <70331360+mattjayyoung@users.noreply.github.com>
2024-02-03 10:36:12 -0500

    February 3rd complete

---
+++ Alan Partridge II - Apple Turnover.js
@@ -0,0 +1,9 @@
+// Alan Partridge II - Apple Turnover - 7kyu (February 3rd)
+
+function apple(x){
+  if (x ** 2 > 1000){
+    return "It's hotter than the sun!!"
+  } else {
+    return 'Help yourself to a honeycomb Yorkie for the glovebox.'
+  }
+}
