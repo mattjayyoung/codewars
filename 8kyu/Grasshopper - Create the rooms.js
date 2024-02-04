11b967beb730eb54509ffdf85a3e93ecf5ef6fad
Matt <70331360+mattjayyoung@users.noreply.github.com>
2024-02-04 14:38:59 -0500

    February 4th complete

---
+++ 8kyu/Grasshopper - Create the rooms.js
@@ -0,0 +1,21 @@
+// Grasshopper - Create the rooms (8kyu) - February 4th
+
+// Add rooms here
+var rooms = {
+    "roomOne": {
+      name: "Family Room",
+      description: "clean",
+      completed: false
+    },
+  "roomTwo":{
+    name: "Kitchen",
+    description: "dirty",
+    completed: true
+  },
+  "roomThree":{
+    name: "Bathroom",
+    description: "wet",
+    completed: false
+  }
+}
+
