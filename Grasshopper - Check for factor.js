3ba2584a14f899b063573ab2e4d52ada2f4230e7
Matt <70331360+mattjayyoung@users.noreply.github.com>
2024-03-21 20:27:00 -0400

    March 21st complete

---
+++ Grasshopper - Check for factor.js
@@ -0,0 +1,11 @@
+// Grasshopper - Check for factor - 8kyu (March 21st)
+
+function checkForFactor (base, factor) {
+  return base % factor === 0 ? true :false
+}
+
+checkForFactor(10,2) // true
+checkForFactor(63,7) // true
+checkForFactor(2450,5) // true
+checkForFactor(24612,3) // true
+  
